33caf2044c6020b76db5c9e54c7ddbf4128361ba
Pavel Stambrecht <pstambrecht@users.noreply.github.com>
2020-11-05 19:07:44 +0100

    Create OneShotEvent.kt

---
+++ OneShotEvent.kt
@@ -0,0 +1,36 @@
+/**
+ * [OneShotEvent] is event which can be executed only once. It wraps data and return them only once.
+ * This class is thread safe - can be used from multiple threads
+ * @param value Event data of type [T]. Can be null.
+ */
+class OneShotEvent<T> constructor(value: T?) {
+    private var data: Any? = value
+    private val tombstone = Any()
+    private val lock = Any()
+
+    /**
+     * Fires the event.
+     * @return given data or null if data has been already fired.
+     *
+     * @see isFired to determine whether event is still fresh
+     */
+    fun fire(): T? {
+        synchronized(lock) {
+            if (data != tombstone) {
+                @Suppress("UNCHECKED_CAST")
+                val value = data as? T
+                data = tombstone
+                return value
+            }
+            return null
+        }
+    }
+
+    /**
+     * Returns whether event has been already fired or not.
+     * @return true if case when event has been already fired, false otherwise
+     */
+    fun isFired(): Boolean = synchronized(lock) {
+        return data == tombstone
+    }
+}
